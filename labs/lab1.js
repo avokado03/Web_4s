ff65f03dbf468f2dafb968f515fb068fd743ad32
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2021-06-23 10:24:01 +0300

    Init

---
+++ labs/lab1.js
@@ -0,0 +1,11 @@
+function zero(){
+    let i = 100;
+    let type = typeof(i);
+    if (type == 'number'){
+        alert('This is number = ' + i)
+    }
+    else{
+        alert('This is not a number')
+    }
+}
+
