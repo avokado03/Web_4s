18c9e8c106deb7c01cfc5fc449432356c1ac1799
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2021-06-23 23:28:52 +0300

    grids - v1

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 const toggle = (el, className) => el.classList.toggle(className);
 
 const toggleLab1 = () => 
-    toggle(document.querySelector('div.lab1'), 'div_visible');
+    toggle(document.querySelector('div.lab1_con'), 'div_visible');
     
 const toggleLab2 = () => 
-    toggle(document.querySelector('div.lab2'), 'div_visible');
+    toggle(document.querySelector('div.lab2_con'), 'div_visible');
 
 
 const clean = (elemClass) => 
