a9bf22eb3196dc0b4f37d7f8a001a2390e91d790
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2021-06-23 22:25:28 +0300

    js + css

--- Script.js
+++ Script.js
@@ -1,4 +1,11 @@
 const toggle = (el, className) => el.classList.toggle(className);
 
-const toggleLab1 = () => toggle(document.querySelector('div.lab1'), 'div_visible');
-const toggleLab2 = () => toggle(document.querySelector('div.lab2'), 'div_visible');
+const toggleLab1 = () => 
+    toggle(document.querySelector('div.lab1'), 'div_visible');
+    
+const toggleLab2 = () => 
+    toggle(document.querySelector('div.lab2'), 'div_visible');
+
+
+const clean = (elemClass) => 
+    document.getElementsByClassName(elemClass)[0].innerHTML = '';
--- labs/lab2.js
+++ labs/lab2.js
@@ -1,11 +1,13 @@
+// печать массива
 function printArray(array){
     let result = '';
-    array.forEach(element => {
-        result += element + ' ';
-    });
+    for(let i = 0; i < array.length; i++){
+        result += array[i] + ' ';
+    }
     return result;
 }
 
+//первая
 function l2_first(){
     let array1 = [3,4,5];
     let array2 = new Array(1,2,5);
@@ -17,6 +19,7 @@ function l2_first(){
     alert(arraysStr);
 }
 
+// вторая
 function l2_second(){
     let array = Array(0, 1, 2, 3, 4, 5);
     let fifth = array[5];
@@ -26,9 +29,11 @@ function l2_second(){
     alert(result);
 }
 
+
+//третья
 let arrayCount = 0;
 const l2_third_listener = function (e){
-    document.getElementsByClassName('l3_third_result').innerText = arrayCount;
+    document.getElementsByClassName('l2_third_result')[0].innerHTML = arrayCount;
 }
 function l2_third(){
     let array = [1,2,3,4,5];
@@ -37,6 +42,58 @@ function l2_third(){
 }
 
 function l2_third_stop(){
-    document.getElementsByClassName('l3_third_result').innerText = "-";
+    document.getElementsByClassName('l2_third_result')[0].innerText = "-";
     document.removeEventListener("mousemove", l2_third_listener);
+}
+
+//четвертая
+function l2_fourth(){
+    let funcCode = printArray.toString();
+    document.getElementsByClassName("l2_fourth_result")[0].innerHTML = funcCode;
+}
+
+// задачи с 5 по 8
+function l2_arrayFunctions(){
+    let arrayStr = ["a", "b", "c"];
+    let arrayNumber = [1, 2, 3];
+
+    let concatenated = arrayStr.concat(arrayNumber);
+    let result = '//Concatenated: ' + printArray(concatenated);
+
+    let last = concatenated.pop();
+    result += '<br>//Pop last: ' + last;
+
+    concatenated.unshift('15');
+    result += '<br>//Push first: ' + concatenated[0] + '<br>';
+
+    result += l2_arrayFunctions;
+    document.getElementsByClassName("l2_fifth_result")[0].innerHTML = result;
+}
+
+//девятая
+function l2_ninth(){
+    alert(new Date());
+}
+
+//десятая
+function l2_tenth(){
+    let options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      };
+    alert(new Date().toLocaleString("ru", options));
+}
+
+//одиннадцатая
+function l2_eleventh(){
+    function getRandomValue(min, max){
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    let random1 = getRandomValue(0, 50);
+    let random2 = getRandomValue(0, 50);
+
+    let result = random1 + ' * ' + random2 + ' = ' + (random1 * random2);
+    alert(result);
 }
