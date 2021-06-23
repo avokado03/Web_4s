07c98973ea7754ca1f7e5b49bdde3ea75240ec8c
Балашова Ангелина <angelina.balashova@simbirsoft.com>
2021-06-23 13:54:51 +0300

    bugfix - lab1

--- labs/lab1.js
+++ labs/lab1.js
@@ -1,24 +1,20 @@
-function zero(i){
-    let type = typeof(i);
-    if (type == 'number'){
+function zero(i) {
+    let type = typeof (i);
+    if (type == 'number') {
         alert('This is number = ' + i)
-    }
-    else if (type == 'string')
-    {
+    } else if (type == 'string') {
         alert('This is string \'' + i + '\'')
-    }
-    else if (type == 'undefined'){
-        alert ('This is undefined')
-    }
-    else{
+    } else if (type == 'undefined') {
+        alert('This is undefined')
+    } else {
         alert('Unknown type')
     }
 }
 
-function first(){
+function first() {
     var zodiac = prompt('Введите знак зодиака: ', '');
 
-    switch (zodiac.toLowerCase()) {
+    switch (zodiac.toLowerCase().trim()) {
         case 'весы':
             alert("Ваш знак ВЕСЫ");
             break;
@@ -39,72 +35,68 @@ function first(){
             alert("Знак не найден");
             break;
     }
+}
 
-    function second(){
-        let forStr = '';
-        let whileStr = '';
-        let doWhileStr = '';
+function second() {
+    let forStr = '';
+    let whileStr = '';
+    let doWhileStr = '';
 
-        let i = 1;
-        while(i <= 40){
-            whileStr += i + ' ';
-            i++;
-        }
-
-        let j = 1;
-        do{
-            doWhileStr += j + ' ';
-        }while(j <= 40);
-
-        for(let i = 1; i <= 40; i++){
-            forStr += i + ' ';
-        }
-
-        let result = "For: " + forStr + "<br/>"
-                   + "While: " + whileStr + "<br/>"
-                   + "DoWhile: " + doWhileStr;
-        alert(result);
+    let i = 1;
+    while (i <= 40) {
+        whileStr += i + ' ';
+        i++;
     }
 
-    function third(){
-        while(true){
-            alert('');
-        }
+    let j = 1;
+    do {
+        doWhileStr += j + ' ';
+        j++;
+    } while (j <= 40);
+
+    for (let i = 1; i <= 40; i++) {
+        forStr += i + ' ';
     }
 
-    function fourth(){
-        let isCorrect = true;
-        while (true){
-            let value = prompt("Введите число: ", '');
-            let condition = value != null && !isNaN(value) && res > 5;
-            if (condition){
-                alert("Ура!");
-                isCorrect = false;
-            }
-            else{
-                alert("Некорректный ввод - попробуйте еще");
-            }
-        }
-    }
+    let result = "For: " + forStr + "\n" +
+        "While: " + whileStr + "\n" +
+        "DoWhile: " + doWhileStr;
+    alert(result);
+}
 
-    function fifth(){
-        let result = '';
-        for (let i = 8; i <= 20; i+2)
-        {
-            result += i + ' ';
-        }
-        alert(result);
-    }
-
-    function sixth(){
-        let result = '';
-        for (let i = 1; i <= 7; i+2)
-        {
-            if (i != 5)
-                result += i + ' ';
-        }
-        alert(result);
+function third() {
+    while (true) {
+        alert('');
     }
 }
 
+function fourth() {
+    let isNotCorrect = true;
+    while (isNotCorrect) {
+        let value = prompt("Введите число: ", '').trim();
+        let condition = (value != null) && (!isNaN(value)) && (value > 5);
+        if (condition) {
+            alert("Ура!");
+            isNotCorrect = false;
+        } else {
+            alert("Некорректный ввод - попробуйте еще");
+        }
+    }
+}
 
+function fifth() {
+    let result = '';
+    for (let i = 8; i <= 20; i = i + 2) {
+        result += i + ' ';
+    }
+    alert(result);
+}
+
+function sixth() {
+    let result = '';
+    for (let i = 1; i <= 7; i = i + 2) {
+        if (i != 5)
+            result += i + ' ';
+    }
+    alert(result);
+}
